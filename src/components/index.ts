38e44cfb69fb25235b614ae9c5c147c4d805eb51
criworks <cri@cri.works>
2026-09-14 18:42:50 -0300

    add: link n button components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from '@/components/ui';
 export * from '@/components/header';
 export * from '@/components/hero';
 export * from '@/components/grid';
---
+++ src/components/ui/index.ts
@@ -0,0 +1,2 @@
+export { default as Link } from '@/components/ui/Link.astro'
+export { default as Button } from '@/components/ui/Button.astro'
