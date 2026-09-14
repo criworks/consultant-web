2f5e004202d945ad8bc33bed5ad2b87998608049
criworks <cri@cri.works>
2026-09-14 15:50:38 -0300

    add: splash loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from '@/components/header';
 export * from '@/components/hero';
 export * from '@/components/grid';
 export * from '@/components/intro';
+export * from '@/components/splash';
